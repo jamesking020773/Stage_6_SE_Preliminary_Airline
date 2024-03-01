09b206009623534388f4547cf76298e8cf2f685d
James King <151979414+jamesking020773@users.noreply.github.com>
2024-03-01 17:09:32 +1100

    Update urls.py

--- airline/urls.py
+++ airline/urls.py
@@ -4,5 +4,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', include("flights.urls")),
-    path('users/', include("users.urls"))
 ]
